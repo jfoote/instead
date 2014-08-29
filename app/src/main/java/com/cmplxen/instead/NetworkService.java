4ef68b96420cc5b30db570340bc9e6dd1eb0a127
Jonathan Foote <jmfoote@loyola.edu>
2014-08-29 16:20:18 -0400

    google places search was always returning roads, so i switched to using a type-based search, which seems to work better

--- app/src/main/java/com/cmplxen/instead/NetworkService.java
+++ app/src/main/java/com/cmplxen/instead/NetworkService.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -37,6 +38,7 @@ public class NetworkService extends IntentService {
     public static final String GET_PLACES_ACTION = "com.cmplxen.instead.intent.action.GET_PLACES_ACTION";
     public static final String GET_PLACES_LATITUDE = "com.cmplxen.instead.intent.action.GET_PLACES_LATITUDE";
     public static final String GET_PLACES_LONGITUDE = "com.cmplxen.instead.intent.action.GET_PLACES_LONGITUDE";
+    private String[] mGooglePlacesTypes = null;
 
     public NetworkService() {
         super("NetworkService");
@@ -85,12 +87,17 @@ public class NetworkService extends IntentService {
             result = EntityUtils.toString(response.getEntity());
             */
 
+            // TODO: do this in init if i switch this to a regular Service (not an IntentService)
+            mGooglePlacesTypes = getResources().getStringArray(R.array.google_places_types);
+            String placesTypesParameter = TextUtils.join("|", mGooglePlacesTypes);
+
             Uri.Builder builder = new Uri.Builder();
             builder.scheme("https").authority("maps.googleapis.com").appendEncodedPath("maps/api/place/nearbysearch/json");
             builder.appendQueryParameter("key", "AIzaSyBuWXfg4Np1TAT5D_MyFdNA6v_JRLwhe1k");
             builder.appendQueryParameter("location", String.valueOf(locLat) + "," + String.valueOf(locLong));
-            builder.appendQueryParameter("radius", "50");
-            // TODO: make sure 50m is OK, and move strings to resources
+            // TODO: move strings to resources, protect key
+            builder.appendQueryParameter("rankby", "distance");
+            builder.appendQueryParameter("types", placesTypesParameter);
 
             HttpClient client = new DefaultHttpClient();
             HttpGet request = new HttpGet(builder.toString());
@@ -104,7 +111,7 @@ public class NetworkService extends IntentService {
             //result = builder.toString();
 
             JSONObject obj = new JSONObject(result);
-            placeName = obj.getJSONArray("results").getJSONObject(0).getString("name");
+            placeName = obj.getJSONArray("results").getJSONObject(0).getString("types");
             // TODO: make sure the first place is the best match
             // TODO: put this parser in standalone method for test, or somehow test this whole method
 
